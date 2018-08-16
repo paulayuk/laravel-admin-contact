74a611f59862c68b862ef07ccd46d00f24abecd0
Paulayuk <ayukpaul1@gmail.com>
2018-08-16 10:07:06 +0000

    initial service provider skeleton added

---
+++ src/ContactFormServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace paulayuk\contactform;
+
+
+use Illuminate\Support\ServiceProvider;
+
+
+class ContactFormServiceProvider extends ServiceProvider {
+	
+
+	public function boot()
+	{
+
+	}
+    
+    public function register()
+	{
+
+	}
+	
+}
